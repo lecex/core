d9b48234fb2f13c19f680b89f1b89b0571d7cc37
bigrocs <bigrocs@qq.com>
2020-02-22 11:19:28 +0800

    自动提交 git 代码

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/config/cmd"
+	cli "github.com/micro/go-micro/client/grpc"
 )
 
 func init() {
@@ -13,6 +14,7 @@ func init() {
 
 // Call 使用默认客户端对服务进行同步调用
 func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.CallOption) error {
+	client.DefaultClient = cli.NewClient()
 	request := client.NewRequest(service, endpoint, req)
 	err := client.Call(ctx, request, rsp, opts...)
 	return err
