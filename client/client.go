9ba73a83c01e6ef61f7c3562e1cd0c2a513c9a22
bigrocs <bigrocs@qq.com>
2020-07-04 09:29:13 +0800

    自动提交 git 代码

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/config/cmd"
+	"github.com/micro/go-micro/v2/util/log"
 )
 
 func init() {
@@ -14,6 +15,12 @@ func init() {
 // Call 使用默认客户端对服务进行同步调用
 func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.CallOption) error {
 	request := client.NewRequest(service, endpoint, req)
-	err := client.Call(ctx, request, rsp, opts...)
-	return err
+	if err := client.Call(ctx, request, rsp, opts...); err != nil {
+		return err
+	}
+	// Stream Close 关闭数据流
+	if defer err := client.Stream.Close(); err != nil {
+		log.Log(err)
+	}
+	return nil
 }
