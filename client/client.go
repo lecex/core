c5d4f45ab4a54dc2f9cd3984eecd1d9578b9e5ec
bigrocs <bigrocs@qq.com>
2020-07-23 16:27:14 +0800

    去掉close

--- client/client.go
+++ client/client.go
@@ -14,10 +14,5 @@ func init() {
 // Call 使用默认客户端对服务进行同步调用
 func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.CallOption) error {
 	request := client.NewRequest(service, endpoint, req)
-	if err := client.Call(ctx, request, rsp, opts...); err != nil {
-		return err
-	}
-	// Stream Close 关闭数据流
-	defer client.Stream.Close()
-	return nil
+	return client.Call(ctx, request, rsp, opts...)
 }
