fd2e274d80a45913f3c1dc5a00cd8a11087a8402
bigrocs <bigrocs@qq.com>
2020-08-31 10:34:26 +0800

    自动提交 git 代码

--- plugins/registry/static/static.go
+++ plugins/registry/static/static.go
@@ -1,64 +1,72 @@
-// Package static is a registry plugin for the micro static
+// Package static provides a multicast dns registry
 package static
 
 import (
+	"fmt"
+
+	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/config/cmd"
 	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/mdns"
 )
-
-type static struct {
-	opts registry.Options
-}
 
 func init() {
 	cmd.DefaultRegistries["static"] = NewRegistry
 }
-
-func configure(s *static, opts ...registry.Option) error {
-	return nil
+func main() {
+	a := NewRegistry
+	fmt.Println(a)
 }
 
-func newRegistry(opts ...registry.Option) registry.Registry {
-	s := &static{
-		opts: registry.Options{},
+type staticRegistry struct {
+	r registry.Registry
+}
+
+func (m *staticRegistry) Init(opts ...registry.Option) error {
+	return m.r.Init(opts...)
+}
+
+func (m *staticRegistry) Options() registry.Options {
+	return m.r.Options()
+}
+
+func (m *staticRegistry) Register(service *registry.Service, opts ...registry.RegisterOption) error {
+	return m.r.Register(service, opts...)
+}
+
+func (m *staticRegistry) Deregister(service *registry.Service) error {
+	return m.r.Deregister(service)
+}
+
+func (m *staticRegistry) GetService(service string) ([]*registry.Service, error) {
+	if selector.DefaultSelector.String() == "static" { // 静态选择器是直接返回服务
+		return []*registry.Service{
+			&registry.Service{Name: service},
+		}, nil
 	}
-	configure(s, opts...)
-	return s
+	return m.r.GetService(service)
 }
 
-func (s *static) Init(opts ...registry.Option) error {
-	return configure(s, opts...)
+func (m *staticRegistry) ListServices() ([]*registry.Service, error) {
+	return m.r.ListServices()
 }
 
-func (s *static) Options() registry.Options {
-	return s.opts
+func (m *staticRegistry) Watch(opts ...registry.WatchOption) (registry.Watcher, error) {
+	return m.r.Watch(opts...)
 }
 
-func (s *static) Register(service *registry.Service, opts ...registry.RegisterOption) error {
-	return nil
+func (m *staticRegistry) String() string {
+	return m.r.String()
 }
 
-func (s *static) Deregister(service *registry.Service) error {
-	return nil
-}
-
-func (s *static) GetService(service string) (services []*registry.Service, err error) {
-	services = append(services, &registry.Service{Name: service})
-	return services, nil
-}
-
-func (s *static) ListServices() ([]*registry.Service, error) {
-	return nil, nil
-}
-
-func (s *static) Watch(opts ...registry.WatchOption) (registry.Watcher, error) {
-	return &staticWatcher{}, nil
-}
-
-func (s *static) String() string {
-	return "static"
-}
-
+// NewRegistry returns a new static registry
 func NewRegistry(opts ...registry.Option) registry.Registry {
-	return newRegistry(opts...)
+	return &staticRegistry{
+		r: registry.NewRegistry(opts...),
+	}
+}
+
+// Domain sets the mdnsDomain
+func Domain(d string) registry.Option {
+	return mdns.Domain(d)
 }
