b992cc1354a2427b878cb60662938fbb7926f4cf
bigrocs <bigrocs@qq.com>
2020-10-05 08:03:39 +0800

    自动提交 git 代码

--- env/env_test.go
+++ env/env_test.go
@@ -2,12 +2,10 @@ package env
 
 import (
 	"testing"
-
-	"github.com/lecex/core/env"
 )
 
 func TestEnv(t *testing.T) {
-	val := env.Getenv("DB_DRIVER", "123456")
+	val := Getenv("DB_DRIVER", "123456")
 	if val != "123456" {
 		t.Errorf("Database connection failed, %v!", val)
 	}
