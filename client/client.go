0ca52b33784e3b9a655983d0a1c8099f9c763dbf
bigrocs <bigrocs@qq.com>
2021-05-31 11:02:40 +0800

    自动提交 git 代码

--- client/client.go
+++ client/client.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Call 使用默认客户端对服务进行同步调用
-func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.CallOption) error {
-	request := client.NewRequest(service, endpoint, req)
-	return client.Call(ctx, request, rsp, opts...)
+func Call(ctx context.Context, service string, endpoint string, req interface{}, rsp interface{}, opts ...client.RequestOption) error {
+	request := client.NewRequest(service, endpoint, req, opts...)
+	return client.Call(ctx, request, rsp)
 }
